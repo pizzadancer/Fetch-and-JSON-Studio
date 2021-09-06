307b85d6a54689ab8d7efbcb1f512ff78b400ee2
dlm <dannyleematthew@gmail.com>
2021-09-05 20:42:27 -0400

    hit a brickwall trying to sort these astronauts

--- script.js
+++ script.js
@@ -1,26 +1,82 @@
+function sortTimeInSpace(json) {
+  // Display the astronauts sorted from most to least time in space.
+  let originalAstronautObject = {}; 
+  let astroTimeArray = [];
+  for (let i = 0; i < json.length; i++) {
+    originalAstronautObject[json[i].hoursInSpace] = json[i].id;
+    astroTimeArray.push(json[i].hoursInSpace);
+  }
+  console.log(originalAstronautObject)
+  let reversedAstroTimeArray = astroTimeArray.sort().reverse();
+  console.log(reversedAstroTimeArray)
+  let newAstronautIDs = [];
+  for (let hours of reversedAstroTimeArray) {
+    newAstronautIDs.push(originalAstronautObject[hours]);
+  }
+  console.log(newAstronautIDs)
+  return newAstronautIDs
+}
+
 function init () {
   fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
     response.json().then(function(json) {
       // console.log(json);
-      let name = json[0].firstName + " " + json[0].lastName;
-      let hoursInSpace = json[0].hoursInSpace;
-      let active = json[0].active;
-      let skills = json[0].skills.join(", ");
-      let picture = json[0].picture;
-      let profile = document.getElementById("container");
-      profile.innerHTML = `
-        <div class="astronaut">
-          <div class="bio">
-            <h3>${name}</h3>
-            <ul>
-                <li>Hours in space: ${hoursInSpace}</li>
-                <li>Active: ${active}</li>
-                <li>Skills: ${skills}</li>
-            </ul>
+      // let index = 1;
+      let sortedAstronautIDs = sortTimeInSpace(json); 
+      console.log("sortedIDs " + sortedAstronautIDs);
+      // have a list of astronaut ids
+      // need to display them to the webpage
+      console.log(json)
+      for (let id of sortedAstronautIDs) {
+        
+        console.log(typeof id)
+        // let name = json[id].firstName + " " + json[id].lastName;
+        // console.log(name);
+        let hoursInSpace = json[id].hoursInSpace;
+        let active = json[id].active;
+        let skills = json[id].skills.join(", ");
+        let picture = json[id].picture;
+        let profile = document.getElementById("container");
+        profile.innerHTML += `
+          <div class="astronaut">
+            <div class="bio">
+              <h3>${name}</h3>
+              <ul>
+                  <li>Hours in space: ${hoursInSpace}</li>
+                  <li>Active: ${active}</li>
+                  <li>Skills: ${skills}</li>
+              </ul>
+            </div>
+            <img class="avatar" src="${picture}">
           </div>
-          <img class="avatar" src="${picture}">
-        </div>
-      `
+        `
+      }
+
+
+      // for (let index = 0; index < json.length; index++) {
+      //   let id = index;
+      //   let name = json[id].firstName + " " + json[id].lastName;
+      //   let hoursInSpace = json[id].hoursInSpace;
+      //   let active = json[id].active;
+      //   let skills = json[id].skills.join(", ");
+      //   let picture = json[id].picture;
+      //   let profile = document.getElementById("container");
+      //   profile.innerHTML += `
+      //     <div class="astronaut">
+      //       <div class="bio">
+      //         <h3>${name}</h3>
+      //         <ul>
+      //             <li>Hours in space: ${hoursInSpace}</li>
+      //             <li>Active: ${active}</li>
+      //             <li>Skills: ${skills}</li>
+      //         </ul>
+      //       </div>
+      //       <img class="avatar" src="${picture}">
+      //     </div>
+      //   `
+      // }
+      
+       
     });
   });
 }
